523aca35e8cc0daf40a3ceb45bc800d121c8993c
emcombs23 <emcombs.23.sv@gmail.com>
2026-01-16 14:47:08 +0000

    day 2

--- tensers.py
+++ tensers.py
@@ -12,4 +12,24 @@ X = torch.tensor([
 print(X)
 print(X.dim())
 print(X.shape)
-#mprint(X)
+print("Hello")
+mprint(X)
+
+#Addition
+
+A = torch.tensor([
+    [1,3],
+    [5,2]
+    ])
+B = torch.tensor([
+    [2,7],
+    [10,1]
+    ])
+
+mprint(A+B)
+
+#Hadamard Multiplication
+mprint(A*B)
+
+#Scalar Multiplication
+mprint(4*A)
